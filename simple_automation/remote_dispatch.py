03e9af8f26dddf6372189f1a323e4692ffbce41a
oddlama <oddlama@oddlama.org>
2021-02-06 17:49:38 +0100

    Fixed not using the bytes buffer interface at remote_dispatch.py. Now there are no interpreted strings anywhere.

--- simple_automation/remote_dispatch.py
+++ simple_automation/remote_dispatch.py
@@ -8,25 +8,38 @@ import subprocess
 # to our remote hosts
 script_path = os.path.realpath(__file__)
 
+def read_mode():
+    mode = sys.stdin.buffer.readline().decode('utf-8')
+    if not mode:
+        return None
+    # Strip newline
+    return mode[:-1]
+
 def write_mode(mode):
-    sys.stdout.write(mode)
-    sys.stdout.write('\n')
-    sys.stdout.flush()
+    sys.stdout.buffer.write(mode.encode('utf-8'))
+    sys.stdout.buffer.write(b'\n')
+    sys.stdout.buffer.flush()
+
+def write_data(data):
+    sys.stdout.buffer.write(str(len(data)).encode('utf-8'))
+    sys.stdout.buffer.write(b'\n')
+    sys.stdout.buffer.write(data)
+    sys.stdout.buffer.flush()
 
 def write_str(s):
-    sys.stdout.write(str(len(s)))
-    sys.stdout.write('\n')
-    sys.stdout.write(s)
-    sys.stdout.flush()
+    write_data(s.encode('utf-8'))
 
 def read_len():
-    l = int(sys.stdin.readline())
+    l = int(sys.stdin.buffer.readline().decode('utf-8'))
     if l < 0 or l > 16*1024*1024*1024:
         exit(2)
     return l
 
+def read_data():
+    return sys.stdin.buffer.read(read_len())
+
 def read_str():
-    return sys.stdin.read(read_len())
+    return read_data().decode('utf-8')
 
 def read_str_list():
     xs = []
@@ -56,14 +69,15 @@ class Dispatcher:
 
     def handle_set_input(self):
         # Set input
-        self.execution_settings.input = read_str()
+        self.execution_settings.input = read_data()
         write_mode("ok")
 
     def run_command(self, command):
+        # TODO -vv should enable this
         #print(f"executing command={command} umask={self.execution_settings.umask} user={self.execution_settings.user}", file=sys.stderr, flush=True)
         # TODO become user
         os.umask(self.execution_settings.umask)
-        input = None if self.execution_settings.input is None else self.execution_settings.input.encode('utf-8')
+        input = None if self.execution_settings.input is None else self.execution_settings.input
         return subprocess.run(command, input=input, capture_output=True)
 
     def handle_exec(self):
@@ -74,8 +88,14 @@ class Dispatcher:
         # Return output and status
         write_mode("ok")
         write_str(completed_command.stdout.decode('utf-8'))
+        # TODO -vvv should enable this too
+        #print(f"stdout: {completed_command.stdout}", file=sys.stderr, flush=True)
         write_str(completed_command.stderr.decode('utf-8'))
+        # TODO -vvv should enable this too
+        #print(f"stderr: {completed_command.stderr}", file=sys.stderr, flush=True)
         write_str(str(completed_command.returncode))
+        # TODO -vvv should enable this too
+        #print(f"rc: {str(completed_command.returncode)}", file=sys.stderr, flush=True)
 
         # Reset settings for next command
         execution_settings = ExecutionSettings()
@@ -85,15 +105,11 @@ class Dispatcher:
         os.chdir(os.path.dirname(script_path))
 
         while True:
-            # Read next mode
-            mode = sys.stdin.readline()
-
-            # End script on EOF
+            # Read next mode, but end script on EOF
+            mode = read_mode()
             if not mode:
                 return
 
-            # Strip the newline
-            mode = mode[:-1]
             if mode == "user":
                 self.handle_set_user()
             elif mode == "umask":
