7ad9ce994e574a4147db7c447e46ab95681e0005
oddlama <oddlama@oddlama.org>
2021-02-23 16:05:42 +0100

    Clarified docs for new transaction utils.

--- simple_automation/transactions/utils.py
+++ simple_automation/transactions/utils.py
@@ -43,6 +43,24 @@ def resolve_mode_owner_group(context: Context, mode, owner, group, fallback_mode
     """
     Canonicalize mode, owner and group. If any of them is None, the respective
     variable will be replaced with the context default.
+
+    Parameters
+    ----------
+    context : Context
+        The host execution context
+    mode : int
+        The mode to canonicalize. May be None.
+    owner : str
+        User id or name for the owner. User will be verified as existing on the remote.
+    group : str
+        Group id or name for the group. Group will be verified as existing on the remote.
+    fallback_mode : int
+        The fallback_mode to canonicalize in case mode = None. Usually set to either context.file_mode or context.dir_mode.
+
+    Returns
+    -------
+    (str, str, str)
+        A tuple of (resolved_mode, resolved_owner, resolved_group)
     """
     # Resolve mode to string
     resolved_mode = _mode_to_str(fallback_mode if mode is None else mode)
@@ -66,7 +84,19 @@ def resolve_mode_owner_group(context: Context, mode, owner, group, fallback_mode
 
 def remote_stat(context, path):
     """
-    Returns (file_type, mode, owner, group) tuple for a given remote path.
+    Runs stat on the given remote path.
+
+    Parameters
+    ----------
+    context : Context
+        The host execution context
+    path : str
+        The path to run stat on.
+
+    Returns
+    -------
+    (str, str, str, str)
+        A tuple of (file_type, mode, owner, group)
     """
     stat = context.remote_exec(["stat", "-c", "%F;%a;%u;%g", path])
     if stat.return_code == 0:
@@ -77,8 +107,19 @@ def remote_stat(context, path):
 
 def remote_sha512sum(context: Context, path: str):
     """
-    Returns the hexlified sha512sum of the path on the remote host,
-    or None if an error occurred.
+    Fetch the sha512sum of a remote file.
+
+    Parameters
+    ----------
+    context : Context
+        The host execution context
+    path : str
+        The path to the remote file.
+
+    Returns
+    -------
+    str
+        The hexlified sha512sum of the path on the remote host, or None if an error occurred.
     """
     sha512sum = context.remote_exec(["sha512sum", "-b", path])
     if sha512sum.return_code == 0:
@@ -90,6 +131,30 @@ def remote_upload(context: Context, get_content, title: str, name: str, dst: str
     Calls get_content and saves the resulting string as a file on the remote host at dst.
     No arguments will be templated, this is task of the calling function.
     Optionally accepts file mode, owner and group, if not given, context defaults are used.
+
+    Parameters
+    ----------
+    context : Context
+        The host execution context
+    get_content : Callable[[],str]
+        A function that is called to get the content that should be uploaded.
+    title : str
+        The title for the generated transaction
+    name : str
+        The name for the generated transaction
+    dst : str
+        The remote destination for the uplaoded file
+    mode : int, optional
+        The new file mode. Defaults the current context file creation mode.
+    owner : str, optional
+        The new file owner. Defaults the current context owner.
+    group : str, optional
+        The new file group. Defaults the current context group.
+
+    Returns
+    -------
+    str
+        The hexlified sha512sum of the path on the remote host, or None if an error occurred.
     """
     with context.transaction(title=title, name=name) as action:
         mode, owner, group = resolve_mode_owner_group(context, mode, owner, group, context.file_mode)
