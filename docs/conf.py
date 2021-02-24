13bdbb512367477e375fed71f0eb6fe69f8e15b7
oddlama <oddlama@oddlama.org>
2021-02-24 13:27:56 +0100

    Added debug print for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+print(os.path.realpath('..'))
+print(os.path.realpath('.'))
 
 
 # -- Project information -----------------------------------------------------
