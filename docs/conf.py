e151150569c8efb24bb729f7a0b83a5006b588c3
oddlama <oddlama@oddlama.org>
2021-02-24 14:16:49 +0100

    Try installing via setup py on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import subprocess
-print("----------------------------------")
-subprocess.run(["ls", "-laF"])
-print("----------------------------------")
-subprocess.run(["ls", "-laF", ".."])
-print("----------------------------------")
 
 
 # -- Project information -----------------------------------------------------
